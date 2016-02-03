d28a87fef872b6462ade3db1570e86cffe414b5f
Ross Harrison <rtharrison86@gmail.com>
2016-02-03 00:31:29 -0500

    step 1a completed

--- source/porter_stemmer.d
+++ source/porter_stemmer.d
@@ -1,9 +1,27 @@
 module porter_stemmer;
 
 import std.typecons;
+import std.string;
+import std.array;
 
-string stem(in string s){
+string stem(in string inS){
+    string s = inS.toLower();
+
+    //step 1a
+    if(s[$-4..$] == "sses"){
+        s.length -= 2;
+        replaceInPlace(s, s.length-2, s.length, "ss");
+    } else if(s[$-3..$] == "ies"){
+        s.length -= 2;
+        replaceInPlace(s, s.length-1, s.length, "i");
+    } else if(s[$-2..$] == "ss"){
+    } else if(s[$-1..$] == "s"){
+        s.length --;
+    }
+
     return s;
+
+
 }
 
 unittest {
