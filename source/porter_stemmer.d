83409e3baea337dc2368debb8cffa0adcb67fd33
Ross Harrison <rtharrison86@gmail.com>
2016-02-04 00:04:29 -0500

    add is vowel method

--- source/porter_stemmer.d
+++ source/porter_stemmer.d
@@ -3,8 +3,9 @@ module porter_stemmer;
 import std.typecons;
 import std.string;
 import std.array;
+import std.stdio;
 
-string stem(in string inS){
+string stem(T)(in T inS){
     string s = inS.toLower();
 
     //step 1a
@@ -20,17 +21,44 @@ string stem(in string inS){
     }
 
     return s;
-
-
 }
+
+
+/*
+  Returns true if the letter at
+  the passed index is a vowel
+*/
+bool isVowel(T)(in T word, int index){
+  switch(word[index]){
+    case 'a':
+    case 'A':
+    case 'e':
+    case 'E':
+    case 'i':
+    case 'I':
+    case 'o':
+    case 'O':
+    case 'u':
+    case 'U':
+        return true;
+    case 'y':
+    case 'Y':
+        return index > 0 && !isVowel(word, index-1);
+    default:
+        return false;
+
+  } 
+}
+
 
 unittest {
     // cases are just pairs onf expected outputs
     auto cases = [
+        // 1a
         tuple("caresses", "caress"),
         tuple("ties", "ti"),
         tuple("cats", "cat"),
-        tuple("ponies", "poni")
+        tuple("ponies", "poni"),
     ];
 
     foreach(c; cases){
@@ -38,3 +66,24 @@ unittest {
             "Expected '"~c[0]~"'to stem to '"~c[1]~"'");
     }
 }
+
+unittest {
+    auto cases = [
+      tuple("caresses", 0, false),
+      tuple("caresses", 1, true),
+      tuple("caresses", 3, true),
+      tuple("caresses", 4, false),
+      tuple("caresses", 6, true),
+      tuple("cray", 3, false),
+      tuple("try", 2, true),
+      tuple("yak", 0, false)
+    ];
+
+    foreach(c; cases){
+      auto msg = c[2] ? "Expected '"~c[0][c[1]]~"' in '"~c[0]~"' to be a vowel " :
+                        "Expected '"~c[0][c[1]]~"' in '"~c[0]~"' not to be a vowel ";
+      assert(isVowel(c[0], c[1]) == c[2], msg);
+    }
+
+}
+
